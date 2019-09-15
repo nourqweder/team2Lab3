8d6c3a7f482ad62642d5626b12f947fe9bfb5524
Nour Elhouda Qweder <nouqw898@student.liu.se>
2019-09-16 00:54:24 +0200

    start a new project with public repository

---
+++ Lab3AdvR/R/euclidean.R
@@ -0,0 +1,24 @@
+#' The Euclidean Algorithm is a technique used for finding the Greatest Common divisor of two integers.
+#'
+#' @param x A number.
+#' @param y A number.
+#' @return The sum of \code{x} and \code{y}.
+#' @examples
+#' euclidean(123612, 13892347912)
+#' euclidean(100, 1000)
+#' 
+#' @references 
+#' #' \href{https://en.wikipedia.org/wiki/Euclidean_algorithm}{Euclidean}
+
+#' @export
+
+
+#1.1.1 euclidean()
+euclidean <- function(x, y){
+  while (y != 0) {
+    z = x %% y
+    x = y
+    y = z
+  }
+  return (x)
+}
---
+++ Lab3AdvR/R/wiki_graph.R
@@ -0,0 +1,12 @@
+#' Wiki_graph
+#'
+#' A dataframe that contains the nodes and the weights between them - a simulation of the graph from wikipedia
+#'
+#' @format A data frame with 18 rows and 3 variables:
+#' \describe{
+#'   \item{v1}{node}
+#'   \item{v2}{node}
+#'   \item{w}{weight between v1 and v2 nodes}
+#' }
+#' @source \url{https://en.wikipedia.org/wiki/Dijkstra\%27s_algorithm}
+"wiki_graph"
