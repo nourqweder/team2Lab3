2746f3a6c784d40bd2c4ee61e0109da312fdc008
Nour Elhouda Qweder <nouqw898@student.liu.se>
2019-09-16 12:06:59 +0200

    - update license in DESCRIPTION - add comments in second algorithem

--- Lab3AdvR/R/dijkstra.R
+++ Lab3AdvR/R/dijkstra.R
@@ -17,17 +17,21 @@
 
 dijkstra <- function(graph, init_node)
 {
+  # Input validation
   stopifnot(is.data.frame(graph),is.numeric(init_node), (colnames(graph) == c("v1","v2","w")), (ncol(graph)==3), 
             (init_node <= max(graph[1:2])),
-            (init_node %in% graph[,1]), is.atomic(init_node) & length(init_node) == 1
+            (init_node %in% graph$v1),(init_node %in% graph$v1), is.atomic(init_node) & length(init_node) == 1
   )
-  #  graph<- wiki_graph
-  # init_node <- 1
+   #graph<- wiki_graph
+   #init_node <- 1
+  # Algorithem
+  #1 create a list of unique nodes
   node_list <- unique(graph[,1])
   weight <- c()
   #print(weight)
   for (node in node_list)
   {
+    #2 Set the distance to to init_node to 0 and set the current_node to init_node
     if (node == init_node)
     {
       weight[node] <- 0
@@ -37,16 +41,22 @@ dijkstra <- function(graph, init_node)
       weight[node] <- Inf   #This is an inifinity number
     }
   }
-  
+  #3 Go through all unvisited node and calculate the minimum distance
   while (length(node_list)>0)
   {
+    #4 
     current_node <- which(weight == min(weight[node_list]))
+    #print(current_node)
     node_list <- node_list[! node_list %in% current_node]
-    for (neighbor in graph[graph$v1 == current_node,]$v2)
+    #print(node_list)
+    # Only consider unvisited and the next to be
+    for (unvisited in graph[graph$v1 == current_node,]$v2)
     {
-      dist <- graph[,3][which(graph[,1]==current_node & graph[,2]==neighbor)] + weight[current_node]
-      current_dist <- weight[neighbor]
-      ifelse((dist <= current_dist), weight[neighbor] <- dist, weight[neighbor] <- current_dist)
+      dist <- graph[,3][which(graph[,1]==current_node & graph[,2]==unvisited)] + weight[current_node]
+     # print(dist)
+      current_dist <- weight[unvisited]
+      #print(current_dist)
+      ifelse((dist <= current_dist), weight[unvisited] <- dist, weight[unvisited] <- current_dist)
     }
   }
   return(weight)
